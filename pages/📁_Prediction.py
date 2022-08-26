f90b2c9627e09f5ceb207dc4c244ef675a4c979c
Akshadakhopade2022 <99347301+Akshadakhopade2022@users.noreply.github.com>
2022-08-26 13:59:14 +0530

    Update 📁_Prediction.py

--- pages/📁_Prediction.py
+++ pages/📁_Prediction.py
@@ -4,7 +4,7 @@ import numpy as np
 
 st.title("Model Prediction")
 st.write('Linear Regression perform more accurately than other models. Therefore we use Linear Regression for prediction.')
-st.image("/content/drive/MyDrive/E_Commerce_Sales/sales_pred.png")
+st.image("sales_pred.png")
     
 x1 = st.slider("Average session Length : ",min_value=25.0,max_value=100.0,step=.1,value=30.0 )
     
@@ -14,7 +14,7 @@ x3 = st.slider("Time spent on Website : ",min_value=30.0,max_value=80.0,step=0.1
     
 x4 = st.slider("Length of Membership : ",min_value=0.1,max_value=10.0,step=0.001,value=1.00)
     
-model = pkl.load(open('/ecommerce-sales-prediction/Linear.pkl','rb'))
+model = pkl.load(open('Linear.pkl','rb'))
     
 if st.button('Predict Sale'):
     x = model.predict(np.array([[x1,x2,x3,x4]]))[0]
